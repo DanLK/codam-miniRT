057c3505bef2c84b3b857313da01fc4fd787f6b5
Hollie Gu <gu.manhan@outlook.com>
2025-08-28 10:48:50 +0200

    catch-up hittables

--- src/hittables/hits.c
+++ src/hittables/hits.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   sphere.c                                           :+:    :+:            */
+/*   hits.c                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dloustal <dloustal@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/15 14:50:48 by dloustal      #+#    #+#                 */
-/*   Updated: 2025/08/21 16:20:55 by dloustal      ########   odam.nl         */
+/*   Updated: 2025/08/27 12:38:50 by dloustal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 The vector abc is to represent the variables a, b and c in the general
 solution for a quadratic equation. abc.x = a, abc.y = b, abc.z = z
 */
-
 bool	hit_sphere(t_object *obj, t_ray ray, double *dist)
 {
 	t_vec	abc;
@@ -27,9 +26,9 @@ bool	hit_sphere(t_object *obj, t_ray ray, double *dist)
 
 	rad = obj->sp.diameter / 2.0;
 	abc.x = dot(ray.dir, ray.dir);
-	abc.y = -2.0 * dot(ray.dir, sum_vec(obj->center, neg_vec_new(ray.origin)));
-	abc.z = dot(sum_vec(obj->center, neg_vec_new(ray.origin)),
-			sum_vec(obj->center, neg_vec_new(ray.origin))) - (rad * rad);
+	abc.y = -2.0 * dot(ray.dir, sub_vec(obj->center, ray.origin));
+	abc.z = dot(sub_vec(obj->center, ray.origin),
+			sub_vec(obj->center, ray.origin)) - (rad * rad);
 	discriminant = abc.y * abc.y - 4 * abc.x * abc.z;
 	if (discriminant < 0)
 		return (false);
@@ -67,7 +66,7 @@ bool	hit_object(t_ray ray, t_object *obj, double *dist)
 		return (hit_sphere(obj, ray, dist));
 	else if (obj->type == PLANE)
 		return (hit_plane(obj, ray, dist));
-	// else if (obj->type == CYLINDER)
-	// 	return (hit_cylinder(obj, ray, dist));
+	else if (obj->type == CYLINDER)
+		return (hit_cylinder(obj, ray, dist));
 	return (false);
 }
